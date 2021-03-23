43e1533926712c0ff4dbf40658f19ff52bb625ed
Sara Valencia <sara.valencia.m@gmail.com>
2021-03-23 15:10:00 +0100

    changed history, added some quizz index if we are going to implement easy js method

--- historyscript.js
+++ historyscript.js
@@ -5,4 +5,21 @@ function onClickMenu(){
     document.getElementById("menu-bg").classList.toggle("change-bg");
     
     }
-    
+  
+    var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
